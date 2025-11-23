870f8e0933f26323f9ffc54ad4473f3ec4ead0c2
Able <abl3theabove@gmail.com>
2025-11-23 03:02:07 -0600

    Adding basic debugging plugins

---
+++ src/dbg.rs
@@ -0,0 +1,55 @@
+/// A module for debuging and similar stuff.
+use bevy::{
+    prelude::*,
+    render::view::screenshot::{Capturing, Screenshot, save_to_disk},
+    window::SystemCursorIcon,
+    winit::cursor::CursorIcon,
+};
+
+pub struct DbgPlugin;
+impl Plugin for DbgPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (screenshot_on_spacebar, screenshot_saving));
+    }
+}
+
+pub fn screenshot_on_spacebar(mut commands: Commands, input: Res<ButtonInput<KeyCode>>) {
+    use chrono::prelude::*;
+
+    let local: DateTime<Local> = Local::now();
+
+    let time_fmt = format!(
+        "./assets/local/screenshot-{}-{}-{}-{}-{}-{}-{}.png",
+        local.year(),
+        local.month(),
+        local.day(),
+        local.hour(),
+        local.minute(),
+        local.second(),
+        local.nanosecond()
+    );
+
+    if input.just_pressed(KeyCode::Space) {
+        let path = time_fmt;
+        commands
+            .spawn(Screenshot::primary_window())
+            .observe(save_to_disk(path));
+    }
+}
+pub fn screenshot_saving(
+    mut commands: Commands,
+    screenshot_saving: Query<Entity, With<Capturing>>,
+    window: Single<Entity, With<Window>>,
+) {
+    match screenshot_saving.iter().count() {
+        0 => {
+            commands.entity(*window).remove::<CursorIcon>();
+        }
+        x if x > 0 => {
+            commands
+                .entity(*window)
+                .insert(CursorIcon::from(SystemCursorIcon::Progress));
+        }
+        _ => {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
 use bevy::prelude::*;
 
+pub mod dbg;
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(dbg::DbgPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
+
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::default())),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
